67c5f39af7d1bf6da8a5a21b8e9cef6776d04a37
Marcus Dev <163601890+MarcusDevinicius@users.noreply.github.com>
2025-01-15 17:28:27 -0300

    Iniciando

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section')
+const windowMetade = window.innerHeight * 0.6;
+
+function animaSrcoll() {
+sections.forEach((section) => {
+    const topSec = section.getBoundingClientRect().top - windowMetade;
+    if (topSec < 0) {
+        section.classList.add('ativo');
+    } else {
+        section.classList.remove('ativo')
+    }
+})
+}
+window.addEventListener('scroll', animaSrcoll)
