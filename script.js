06c5512d23d9d62dbe62b64bace650d0551ac42e
Marcus Dev <163601890+MarcusDevinicius@users.noreply.github.com>
2025-01-22 10:11:59 -0300

    Atualizando o JavaScript

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const sections = document.querySelectorAll('section')
 const windowMetade = window.innerHeight * 0.6;
 
 function animaSrcoll() {
-sections.forEach((section) => {
-    const topSec = section.getBoundingClientRect().top - windowMetade;
-    if (topSec < 0) {
-        section.classList.add('ativo');
-    } else {
-        section.classList.remove('ativo')
-    }
-})
+    sections.forEach((section) => {
+        const topSec = section.getBoundingClientRect().top - windowMetade;
+        if (topSec < 0) {
+            section.classList.add('ativo');
+        } else {
+            section.classList.remove('ativo')
+        }
+    })
 }
 window.addEventListener('scroll', animaSrcoll);
 
